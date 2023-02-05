9a68a51d6e5e72f6ca1e7e1252e275efc6c0623d
Md. Alamin Ridoy <105815589+Alaminswe@users.noreply.github.com>
2023-02-06 02:19:45 +0600

    first commit

---
+++ insertion.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <math.h>
+
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+int main()
+{
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    /*
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    int i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    */
+
+    insertionSort(arr, n);
+    printArray(arr, n);
+
+    return 0;
+}
